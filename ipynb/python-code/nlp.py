78a79fa734fe8668110f26d4a1eee5e018c4dc8c
ShayneNg <phamhai1986@yahoo.com>
2019-05-17 07:13:42 +1000

    create model spacy

--- ipynb/python-code/nlp.py
+++ ipynb/python-code/nlp.py
@@ -1,113 +1,120 @@
+import re
 import nltk
 import unicodedata
 import contractions
 from bs4 import BeautifulSoup
 from nltk.tokenize.toktok import ToktokTokenizer
-from nltk.stem import SnowballStemmer, PorterStemmer, WordNetLemmatizer
+from nltk.stem import SnowballStemmer, WordNetLemmatizer
 
 class nlp(object):
     
     def __init__(self, text, stopwords):
         self.text = text
+        self.text = self._remove_special_characters()
         self.stopword_list = stopwords
         self.lemmatizer = WordNetLemmatizer()
-        self.tokens = [token.strip() for token in ToktokTokenizer().tokenize(text)]
-    
+        self.result = ''
+        #self.tokens = [token.strip() for token in ToktokTokenizer().tokenize(text)]
+ 
+
     # strip html METHOD
-    @staticmethod
-    def _strip_html(doc):
+    #@staticmethod
+    def _strip_html(self):
         
-        soup = BeautifulSoup(doc, "html.parser")
+        soup = BeautifulSoup(self.text, "html.parser")
         result = soup.get_text()
+        #result = re.sub(r"(<[+>]>)","",
+        
+        return result
+ 
+
+    # remove accented character METHOD
+    #@staticmethod
+    def _remove_accent_character(self):
+        
+        result = unicodedata.normalize('NFKD', self.text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
         return result
     
-    # remove accented character METHOD
-    @staticmethod
-    def _remove_accent_character(doc):
-        
-        result = unicodedata.normalize('NFKD', doc).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-        return result
     
     # expand contractions METHOD
-    @staticmethod
-    def _expand_contractions(doc):
+    #@staticmethod
+    def _expand_contractions(self):
         
-        return contractions.fix(doc)
-    
-    # sub-function: _expand_contractions()
-    # expand matched contractions METHOD
-    @staticmethod
-    def __expand_contractions_match(self, contraction):
-        
-        match = contraction.group[0]
-        first_char = match[0]
+        return contractions.fix(self.text)
+
     
     # remove special characters METHOD
-    @staticmethod
-    def _remove_special_characters(doc, remove_digits=True):
+    #@staticmethod
+    def _remove_special_characters(self, remove_digits=True):
         
-        pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
-        result = re.sub(pattern, '', doc)
+        # remove special characters
+        pattern = r"([^a-zA-z0-9\s])" if not remove_digits else r"([^a-zA-z\s])"
+        result = re.sub(pattern, '', self.text)
+        
+        # remove multiple lines & spaces.
+        result = re.sub(r"([\r|\n|\r\n]+)"," ",result)
+        result = re.sub(r'( +)', ' ', result)
+        result = re.sub(r'([{.(-)!}])',' ',result)
+        
+        result = result.lower()
+
         return result
     
+    
     # Stemming METHOD
-    @staticmethod
-    def _stemming(doc, stem=True):
+    #@staticmethod
+    def _stemming(self, stem=False):
         
         if stem:
             stemmer = SnowballStemmer("english")
         else:
-            stemmer = PorterStemmer("english")
+            stemmer = nltk.porter.PorterStemmer()
             
-        result = ' '.join([stemmer.stem(word) for word in doc.split()])
-        return result
+#         result = ' '.join([stemmer.stem(word) for word in self.text.split()])
+
+        return stemmer.stem(self._lemmatization())
+    
     
     # Lemmatization METHOD
-    @staticmethod
-    def _lemmatization(doc,lemmatizer=lemmatizer):
+    #@staticmethod
+    def _lemmatization(self):
         
-        result = lemmatizer.lemmatize(doc,pos='v')
+        result = self.lemmatizer.lemmatize(self.text,pos='v')
                                       
         return result
     
+    
     # remove stopwords
-    @staticmethod
-    def _remove_stopwords(tokens,stopword_list):
-        #tokens = tokenizer.tokenize(text)
-        #tokens = [token.strip() for token in tokens]
+    #@staticmethod
+    def _remove_stopwords(self):
+        tokens = tokenizer.tokenize(self.text)
+        tokens = [token.strip() for token in tokens]
         
-        result = [token for token in tokens if token.lower() not in stopword_list]
+        result = [token for token in tokens if token.lower() not in self.stopword_list]
         result = ' '.join(result)
         
         return result
 
+    
     # normalize METHOD
     def normalize(self):
         
-        result = self.text
+        self.result = self.text
         
         # strip HTML 
         # clear newlines & extra-whitespace
-        result = self._strip_html(result)
-        result = re.sub("[\r|\n|\r\n]+"," ",result)
-        result = re.sub(' +', ' ', result)
+        self.result = self._strip_html()        
         
-        # remove accented character
-        result = self._remove_accent_character(result)
+        # remove accented characters
+        self.result = self._remove_accent_character()
         
         # expand contractions
-        result = self._expand_contractions(result)
-        
-        # lowercase text
-        result = result.lower()
+        self.result = self._expand_contractions()
         
         # Lemmatizer
-        result = self._lemmatization(result)
-        
-        # remove special characters
-        result = self._remove_special_characters(result)
-        
+        self.result = self._lemmatization()
+               
         # remove stop words
-        result = self._remove_stopwords(self.tokens,self.stopword_list)
+        self.result = self._remove_stopwords()
         
-        return result
+        return self.result
