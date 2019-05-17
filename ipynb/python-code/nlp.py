1a04c0d69075c1f0f872fc90b6e88adefc9215d8
sealbamboo <phamhai1986@yahoo.com>
2019-05-17 16:04:38 +1000

    update trainging context

--- ipynb/python-code/nlp.py
+++ ipynb/python-code/nlp.py
@@ -8,12 +8,14 @@ from nltk.stem import SnowballStemmer, WordNetLemmatizer
 
 class nlp(object):
     
-    def __init__(self, text, stopwords):
+    def __init__(self, text, tokenizer, stopwords):
         self.text = text
-        self.text = self._remove_special_characters()
+        self.result = text
+        self.tokenizer = tokenizer
+
         self.stopword_list = stopwords
         self.lemmatizer = WordNetLemmatizer()
-        self.result = ''
+
         #self.tokens = [token.strip() for token in ToktokTokenizer().tokenize(text)]
  
 
@@ -21,19 +23,19 @@ class nlp(object):
     #@staticmethod
     def _strip_html(self):
         
-        soup = BeautifulSoup(self.text, "html.parser")
-        result = soup.get_text()
-        #result = re.sub(r"(<[+>]>)","",
+        #soup = BeautifulSoup(self.text, "html.parser")
+        #result = soup.get_text()
+        self.result = re.sub(r"(<[^>]+>)"," ",self.result)
         
-        return result
+        return self.result
  
 
     # remove accented character METHOD
     #@staticmethod
     def _remove_accent_character(self):
         
-        result = unicodedata.normalize('NFKD', self.text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-        return result
+        self.result = unicodedata.normalize('NFKD', self.text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        return self.result
     
     
     # expand contractions METHOD
@@ -45,20 +47,26 @@ class nlp(object):
     
     # remove special characters METHOD
     #@staticmethod
-    def _remove_special_characters(self, remove_digits=True):
+    def _remove_special_characters(self, remove_digits=False):
         
+        self.result = unicodedata.normalize('NFKD', self.result).encode('ascii', 'ignore').decode('utf-8')
         # remove special characters
-        pattern = r"([^a-zA-z0-9\s])" if not remove_digits else r"([^a-zA-z\s])"
-        result = re.sub(pattern, '', self.text)
-        
-        # remove multiple lines & spaces.
-        result = re.sub(r"([\r|\n|\r\n]+)"," ",result)
-        result = re.sub(r'( +)', ' ', result)
-        result = re.sub(r'([{.(-)!}])',' ',result)
-        
-        result = result.lower()
+        # pattern = r"([^a-zA-z0-9\s\'])" if not remove_digits else r"([^a-zA-z\s\'])"
+        # self.result = re.sub(pattern, ' ', self.result)
+        if remove_digits:
+            self.result = re.sub(r"([^a-zA-z0-9\s\'])"," ",self.result)
+        else:
+            self.result = re.sub(r"([^a-zA-z\s])"," ",self.result)
 
-        return result
+        # remove multiple lines & spaces.
+        self.result = re.sub(r"([\r|\n|\r\n]+)"," ",self.result)
+        self.result = re.sub(r"( +)", " ", self.result)
+        self.result = re.sub(r"([{.(-)!}])"," ",self.result)
+        
+        # lowercase all words
+        self.result = self.result.lower()
+
+        return self.result
     
     
     # Stemming METHOD
@@ -79,9 +87,9 @@ class nlp(object):
     #@staticmethod
     def _lemmatization(self):
         
-        result = self.lemmatizer.lemmatize(self.text,pos='v')
+        self.result = self.lemmatizer.lemmatize(self.result,pos='v')
                                       
-        return result
+        return self.result
     
     
     # remove stopwords
@@ -90,10 +98,10 @@ class nlp(object):
         tokens = tokenizer.tokenize(self.text)
         tokens = [token.strip() for token in tokens]
         
-        result = [token for token in tokens if token.lower() not in self.stopword_list]
-        result = ' '.join(result)
+        self.result = [token for token in tokens if token.lower() not in self.stopword_list]
+        self.result = ' '.join(self.result)
         
-        return result
+        return self.result
 
     
     # normalize METHOD
@@ -111,8 +119,11 @@ class nlp(object):
         # expand contractions
         self.result = self._expand_contractions()
         
-        # Lemmatizer
-        self.result = self._lemmatization()
+        # remove special characters
+        self.result = self._remove_special_characters()
+        
+        # _stemming
+        self.result = self._stemming()
                
         # remove stop words
         self.result = self._remove_stopwords()
